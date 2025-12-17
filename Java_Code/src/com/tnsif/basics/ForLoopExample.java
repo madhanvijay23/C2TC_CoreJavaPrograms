c93bd58d78aa38fdf6bd2f5bb8e5e6797bb845d8
madhanvijay23 <madhanvijay2325@gmail.com>
2025-12-17 09:51:40 +0530

    commit 1

---
+++ Java_Code/src/com/tnsif/basics/ForLoopExample.java
@@ -0,0 +1,15 @@
+package com.tnsif.basics;
+
+public class ForLoopExample {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=1000 ; i--)
+		{
+			System.out.println("value of i:");
+			System.out.println(i);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
